b8bb696b17d8387fb2c91e86d92ff98a27f00b1c
DESKTOP-ROETF6N\javastudy <kmdadoo@naver.com>
2024-07-23 17:14:57 +0900

    SpringBoot 9장 ~ 14장 SimpleBBS

---
+++ springboot.sql
@@ -0,0 +1,41 @@
+-- 스프링부트 13장
+DROP TABLE myuser;
+CREATE TABLE myuser(
+    id varchar2(20),
+    name varchar2(10)
+);
+-- 샘플 데이터 직접 추가하기
+insert into myuser values ( 'test1', '홍길동1');
+insert into myuser values ( 'test2', '홍길동2');
+insert into myuser values ( 'test3', '홍길동3');
+commit;
+
+-- 14장
+drop table simple_bbs;
+create table simple_bbs (
+    id number(4) primary key,
+    writer varchar2(100),
+    title varchar2(100),
+    content varchar2(100)
+);
+
+drop sequence simple_bbs_seq;
+-- id 컬럼의 값을 자동으로 1씩 증가시키기 위해서 
+-- 오라클의 시퀀스 기능을 사용하고 있다
+create sequence simple_bbs_seq;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
